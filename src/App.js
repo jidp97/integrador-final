7ad074906efdbf724684522b508e36184c1b8d7e
José Ismael <39838774+jidp97@users.noreply.github.com>
2024-04-12 01:04:30 -0400

    importar signupform

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import SignUpForm from "./components/SignUpForm";
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
